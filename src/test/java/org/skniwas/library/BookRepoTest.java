87f44035b4fc325c41ae1984f024d5910c50401d
Adam <adam.ciesielczuk@gmail.com>
2020-01-18 20:53:03 +0100

    refactor: refactoring test methods

--- src/test/java/org/skniwas/library/BookRepoTest.java
+++ src/test/java/org/skniwas/library/BookRepoTest.java
@@ -1,38 +1,52 @@
 package org.skniwas.library;
 
-
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skniwas.library.model.Book;
 import org.skniwas.library.model.repository.BookRepositoryImpl;
 import org.skniwas.library.utils.SearchCriteria;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BookRepoTest {
 
     BookRepositoryImpl bookRepository = new BookRepositoryImpl();
 
+    @BeforeEach
+    public void setUp() {
+        bookRepository.build();
+    }
+
     @Test
     public void searchTestByAuthor() {
+        // Given
         List<Book> expectedList = new ArrayList<>();
-        bookRepository.build();
         expectedList.add(new Book("Martin", "Clean Code", true));
         expectedList.add(new Book("Martin", "Clean Architecture", true));
 
-        Assertions.assertEquals(expectedList.toString(), bookRepository.search(new SearchCriteria("Martin", "author")).toString());
+        // When
+        Collection<Book> resultList = bookRepository.search(new SearchCriteria("Martin", "author"));
+
+        // Then
+        assertEquals(expectedList.toString(), resultList.toString());
     }
 
 
     @Test
     public void searchTestByTitle() {
+        // Given
         List<Book> expectedList = new ArrayList<>();
-        bookRepository.build();
         expectedList.add(new Book("Horstmann", "Core Java", true));
 
-        Assertions.assertEquals(expectedList.toString(), bookRepository.search(new SearchCriteria("Core Java", "title")).toString());
-    }
+        // When
+        Collection<Book> resultList = bookRepository.search(new SearchCriteria("Core Java", "title"));
 
+        // Then
+        assertEquals(expectedList.toString(), resultList.toString());
+    }
 
 }
